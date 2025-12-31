38186fa937876d6c50696df8513a969cbca8f76e
minhyuk125423 <alsgurghwls@gmail.com>
2025-12-31 11:33:37 +0900

    복호화 정상작동

--- vigenere.py
+++ vigenere.py
@@ -1,14 +1,98 @@
+# def encode(word,key):
+#     new_word = ""
+#     key = key.lower()
+#     l1 = list('!"#$%&()\'*+,-./')
+#     l2 = list(':;<=>?@')
+#     l3 = list('[\\]^_`')
 
-__author__ = 'yoav'
+#     for i in range(len(word)):
+#         if word[i].isupper():
+#             stddisp = 65
+#             mod = 26
+#         elif word[i].islower():
+#             stddisp = 97
+#             mod = 26
+#         elif word[i].isdigit():
+#             stddisp = 48
+#             mod = 10
+#         elif word[i] in l1:
+#             stddisp = 33
+#             mod = 16
+#         elif word[i] in l2:
+#             stddisp = 58
+#             mod = 7
+#         elif word[i] in l3:
+#             stddisp = 32
+#             mod = 6
+#         print("-------------s")
+#         print(stddisp,mod)
+#         row = ord(word[i]) - stddisp
+#         col = ord(key[i%len(key)]) - stddisp
+#         print(stddisp + (row+col)%mod)
+#         print(row+col)
+#         print((row+col)%mod)
+#         new_word += chr(stddisp + (row+col) % mod)
+#     return new_word
+
+# def decode(word,key):
+#     decoded_word = ""
+#     key = key.lower()
+#     l1 = list('!"#$%&()\'*+,-./')
+#     l2 = list(':;<=>?@')
+#     l3 = list('[\\]^_`')
+#     for i in range(len(word)):
+#         if word[i].isupper():
+#             stddisp = 65
+#             mod = 26
+#         elif word[i].islower():
+#             stddisp = 97
+#             mod = 26
+#         elif word[i].isdigit():
+#             stddisp = 48
+#             mod = 10
+#         elif word[i] in l1:
+#             stddisp = 33
+#             mod = 16
+#         elif word[i] in l2:
+#             stddisp = 58
+#             mod = 7
+#         elif word[i] in l3:
+#             stddisp = 32
+#             mod = 6
+#         row = ord(word[i]) - stddisp
+#         col = ord(key[i%len(key)]) - stddisp
+#         decoded_word += chr(stddisp + (row+col) % mod)
+#     return decoded_word
+
+# def main():
+#     while True:
+#         print("----------------------")
+#         print("1: Encode plain text\n")
+#         print("2: Decode encrypted text\n")
+#         inpt = input("Enter choice: ")
+#         if (inpt.isdigit()):
+#             text = input("Enter text: ")
+#             keyword = input("Enter keyword: ")
+#             if inpt == "1":
+#                 print("Encoded text: " + encode(text,keyword))
+#             elif inpt == "2":
+#                 print("Decoded text: " + decode(text,keyword))
+#             else:
+#                 print("Invalid input")
+#         else:
+#             print("Invalid input")
 
 
+# if __name__ == "__main__":
+#     main()
 
-def encode(word,key):
+def encode(word, key):
     new_word = ""
     key = key.lower()
     l1 = list('!"#$%&()\'*+,-./')
     l2 = list(':;<=>?@')
     l3 = list('[\\]^_`')
+    
     for i in range(len(word)):
         if word[i].isupper():
             stddisp = 65
@@ -28,22 +112,18 @@ def encode(word,key):
         elif word[i] in l3:
             stddisp = 32
             mod = 6
-        print("-------------s")
-        print(stddisp,mod)
         row = ord(word[i]) - stddisp
-        col = ord(key[i%len(key)]) - stddisp
-        print(stddisp + (row+col)%mod)
-        print(row+col)
-        print((row+col)%mod)
-        new_word += chr(stddisp + (row+col) % mod)
+        col = ord(key[i % len(key)]) - stddisp
+        new_word += chr(stddisp + (row + col) % mod)
     return new_word
 
-def decode(word,key):
+def decode(word, key):
     decoded_word = ""
     key = key.lower()
     l1 = list('!"#$%&()\'*+,-./')
     l2 = list(':;<=>?@')
     l3 = list('[\\]^_`')
+    
     for i in range(len(word)):
         if word[i].isupper():
             stddisp = 65
@@ -64,8 +144,8 @@ def decode(word,key):
             stddisp = 32
             mod = 6
         row = ord(word[i]) - stddisp
-        col = ord(key[i%len(key)]) - stddisp
-        decoded_word += chr(stddisp + (row+col) % mod)
+        col = ord(key[i % len(key)]) - stddisp
+        decoded_word += chr(stddisp + (row - col) % mod)
     return decoded_word
 
 def main():
@@ -73,19 +153,19 @@ def main():
         print("----------------------")
         print("1: Encode plain text\n")
         print("2: Decode encrypted text\n")
+        
         inpt = input("Enter choice: ")
-        if (inpt.isdigit()):
+        if inpt.isdigit():
             text = input("Enter text: ")
             keyword = input("Enter keyword: ")
             if inpt == "1":
-                print("Encoded text: " + encode(text,keyword))
+                print("Encoded text: " + encode(text, keyword))
             elif inpt == "2":
-                print("Decoded text: " + decode(text,keyword))
+                print("Decoded text: " + decode(text, keyword))
             else:
                 print("Invalid input")
         else:
             print("Invalid input")
-
 
 if __name__ == "__main__":
     main()
